374677ac7e65031c5246a0ec22a240ef392d7e4c
HarakaRisasi <harakarisasi@gmail.com>
2019-10-06 03:30:25 +1000

    add.ex_001_to_006

---
+++ ex.001_to_xxx.js
@@ -0,0 +1,82 @@
+//ex.001
+    /*
+       Seven different data types is JS
+       1.undefined
+       2.null
+       3.boolean
+       4.string
+       5.symbol
+       6.number
+       7.object
+     */
+
+//ex.002
+    var ourName;
+    var myName;
+
+//ex.003
+    var a;
+    var b = 2;
+    
+    a = 7;
+    b = a;
+
+//ex.004
+    var a = 9;
+
+//ex.005
+    var a;
+    var b;
+    var c;
+
+    a = 5;
+    b = 10;
+    c = "I am a";
+
+//ex.006
+    // Declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+//ex.007
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
