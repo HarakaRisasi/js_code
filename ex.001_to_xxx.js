744cb50680de344e046cf24f068bcc51f52261dd
HarakaRisasi <harakarisasi@gmail.com>
2019-10-07 04:47:30 +1000

    add.ex_xxx_to_ex_026

--- ex.001_to_xxx.js
+++ ex.001_to_xxx.js
@@ -76,7 +76,115 @@
     var quotient = 4.4 / 2.0; 
 
 //ex.016
+    /*
+    Reminder:
+        5 % 2 = 1 
+	because
+	5 / 2 = 2
+	2 * 2 = 4
+	5 - 4 = 1
+    Note
+    The remainder operator is sometimes incorrectly referred to 
+    as the "modulus" operator. It is very similar to modulus, 
+    but does not work properly with negative numbers.
+    */
+
+    var reminder;
+    reminder = 11 % 3;
     
+//ex.017
+    var a = 3;
+    var b = 17;
+    var c = 12;
+
+    a += 12;
+    b += 9;
+    c += 7;
+
+//ex.018
+    var a = 11;
+    var b = 9;
+    var c = 3;
+
+    a -= 6;
+    b -= 15;
+    c -= 1;
+
+//ex.019
+    var a = 5;
+    var b = 12;
+    var c = 4.6;
+
+    a *= 5;
+    b *= 3;
+    c *= 10;
+
+//ex.020
+    var a = 48;
+    var b = 108;
+    var c = 33;
+
+    a /= 12;
+    b /= 4;
+    c /= 11;
+
+//ex.021
+    var firstName = "Alan";
+    var lastName = "Turing";
+    
+    var myFirstName = "Hello";
+    var myLastName = "World";
+
+//ex.022
+    var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//ex.023
+    /*
+       doubleQuoteStr = "This is a string"; 
+       singleQuoteStr = 'This is also a string';
+
+       conversation = 'Finn exclaims to Jake, "Algebraic!"';
+
+       goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; 
+       badStr = 'Finn responds, "Let's go!"'; // Throws an error
+    */
+
+    var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//ex.024
+    /*
+	FirstLine
+	    \SecondLine
+        ThirdLine
+    */
+    var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//ex.025
+    var myStr = "This is the start. " + "This is the end.";
+
+//ex.026
+    var ourStr = "I come first. ";
+    ourStr += "I come second.";
+
+    var myStr = "This is the first sentence. ";
+    myStr += "This is the second sentence."
+
+//ex.027
+
+//ex.107
+
+
+
+
+
+
+
+
+
+
+
+
+var myStr = "This is the start. " + "This is the end.";
 
 
 
@@ -91,18 +199,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+var myStr = "This is the start. " + "This is the end.";
 
 
 
