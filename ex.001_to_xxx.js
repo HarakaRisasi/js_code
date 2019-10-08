efb637c5d7614d011f1e8f0a11633058a61e10a4
HarakaRisasi <harakarisasi@gmail.com>
2019-10-09 06:54:30 +1000

    >>_add.ex_70

--- ex.001_to_xxx.js
+++ ex.001_to_xxx.js
@@ -563,8 +563,53 @@
     }
 
 //ex.067
+    if (val > 5) {
+        result = "Bigger than 5";
+    }else{
+        result = "5 or Smaller";
+    } 
 
-//ex.107
-    	
+//ex.068
+    function testElseIf(val) {
+        if (val > 10) {
+            return "Greater than 10";
+        } 
+        else if (val < 5) {
+            return "Smaller than 5";
+        }else{
+            return "Between 5 and 10";
+        }
+    }
 
-//ex.107
+//ex.069
+    function orderMyLogic(val) {
+        if (val < 5) {
+            return "Less than 5";
+        } else if (val < 10) {
+            return "Less than 10";
+        } else {
+            return "Greater than or equal to 10";
+        }
+    }
+
+//ex.070
+    function testSize(num){
+        if(num < 5){
+            return "Tiny";
+        }else if(num < 10){
+            return "Small";
+        }else if(num < 15){
+            return "Medium";
+        }else if(num < 20){
+            return "Large";
+        }else if(num >= 20){
+            return "Huge";
+        }else{
+            return "Change Me";
+        }
+    }
+
+    console.log(testSize(12));
+
+//ex.071
+//ex.106
