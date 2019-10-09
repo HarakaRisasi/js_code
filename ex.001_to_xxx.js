ad8b8ed43ba5af556b4d58c85e00a473f6ad7544
HarakaRisasi <harakarisasi@gmail.com>
2019-10-10 03:18:50 +1000

    >>_add.ex_83

--- ex.001_to_xxx.js
+++ ex.001_to_xxx.js
@@ -612,4 +612,236 @@
     console.log(testSize(12));
 
 //ex.071
+    /*Strokes	    Return
+        1	        "Hole-in-one!"
+        <= par - 2	"Eagle"
+        par - 1	    "Birdie"
+        par	        "Par"
+        par + 1	    "Bogey"
+        par + 2	    "Double Bogey"
+        >= par + 3	"Go Home!"
+    */
+
+   var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+    function golfScore(par, strokes) {
+        if(par && strokes == 1){
+            return names[0];
+        }else if(par && strokes <= par-2){
+            return names[1];
+        }else if(par && strokes == par-1){
+            return names[2];
+        }else if(par && strokes == par){
+            return names[3];
+        }else if(par && strokes == par + 1){
+            return names[4];
+        }else if(par && strokes == par + 2){
+            return names[5];
+        }else
+            return names[6];
+    }
+    console.log(golfScore(4, 1)); 
+
+//ex.072
+    function caseInSwitch(val){
+        var answer = "";
+
+        switch(val){
+            case 1 : answer = "alpha";
+            break;
+            case 2 : answer = "beta";
+            break;
+            case 3 : answer = "gamma";
+            break;
+            case 4 : answer = "delta";
+        }
+
+        return answer;
+    }
+
+    console.log(caseInSwitch(4));
+
+//ex.073
+    function switchOfStuff(val){
+        var answer = "";
+    
+        switch(val){
+            case "a" : answer = "apple";
+            break;
+            case "b" : answer = "bird";
+            break;
+            case "c" : answer = "cat";
+            break;
+            default : answer = "stuff";
+            break;
+        }
+    
+        return answer;
+    }
+    
+    console.log(switchOfStuff(4));
+
+//ex.074
+    //Multiple Identical Options in Switch Statements
+    function sequentialSizes(val){
+        var answer = "";
+
+        switch(val){
+            case 1:
+            case 2:
+            case 3 : answer = "Low";
+            break;
+            case 4:
+            case 5:
+            case 6: answer = "Mid";
+            break;
+            case 7:
+            case 8:
+            case 9: answer = "High";
+        }
+
+        return answer;
+    }
+
+    console.log(sequentialSizes(4));
+
+//ex.075
+    function chainToSwitch(val){
+        var answer = "";
+
+        switch(val){
+            case "bob": answer = "Marley";
+            break;
+            case 42: answer = "The Answer";
+            break;
+            case 1: answer = "There is no #1";
+            break;
+            case 99: answer = "Missed me by this much!";
+            break;
+            case 7: answer = "Ate Nine"
+        }
+
+        return answer;
+    }
+
+    console.log(chainToSwitch(42));
+
+//ex.076
+    function isLess_hard(a, b) {
+
+        if (a < b) {
+        return true;
+        } else {
+        return false;
+        }
+    }
+
+    //write like a Bool.
+    function isLess(a, b) {
+        return a < b;
+    }
+
+    console.log(isLess(10, 15));
+
+//ex.077
+    function abTest(a, b) {
+        if(a < 0 || b < 0){
+            return undefined;
+        }
+        return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+    console.log(abTest(2,2));
+
+//ex.078
+    var count = 0;
+
+    function cc(card) {
+        if(card > 1 && card < 7){
+            count++;
+        }
+
+        switch(card){
+            case 10:
+            case 'J':
+            case 'Q':
+            case 'K':
+            case 'A': count --;
+        }
+
+        if(count > 0){
+            return(count + ' Bet');
+        }else
+            return(count + ' Hold');
+    }
+
+    console.log(cc());
+
+    cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+//ex.079
+    // Build JavaScript Objects
+    var ourDog = {
+        "name": "Camper",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["everything!"]
+    };
+    
+    var myDog = {
+        name : "Harry",
+        legs : 4,
+        tails : 1,
+        friends : ["Bobby", "Anna-Elisabetta", "Spice"]
+    };
+
+    console.log(myDog.name, myDog.tails);
+
+//ex.080
+    var testObj = {
+        "hat": "ballcap",
+        "shirt": "jersey",
+        "shoes": "cleats"
+    };
+
+    var hatValue = testObj.hat;      
+    var shirtValue = testObj.shirt;
+
+//ex.081
+    //Accessing Object Properties with Bracket Notation
+    var testObj = {
+        "an entree": "hamburger",
+        "my side": "veggies",
+        "the drink": "water"
+    };
+
+    var entreeValue = testObj["an entree"];
+    var drinkValue = testObj["the drink"];
+
+//ex.082
+    //Accessing Objects Properties with Variables
+    var testObj = {
+        12: "Namath",
+        16: "Montana",
+        19: "Unitas"
+    };
+    
+    var playerNumber = 16;
+    var player = testObj[playerNumber];
+
+    console.log(playerNumber);
+    console.log(player);
+
+//ex.083
+    var myDog = {
+        "name": "Coder",
+        "legs": 4,
+        "tails": 1,
+        "friends": ["freeCodeCamp Campers"]
+    };
+
+    myDog.name = "Happy Coder";
+
+    console.log(myDog.name);
+
+//ex.084
+
 //ex.106
