62a864391e6c7a2fd1d0ef74a75d33d6ed8b0e72
HarakaRisasi <harakarisasi@gmail.com>
2019-10-17 02:46:50 +1000

    >>_add.ex_004

--- ES6/es6_ex.1_to_ex.xxx.js
+++ ES6/es6_ex.1_to_ex.xxx.js
@@ -90,5 +90,19 @@ function printManyTimes(str) {
 console.log(printManyTimes("freeCodeCamp"));   
 
 //ex.004
+/*
+const s = [5, 6, 7];
+//s = [1, 2, 3];
+//console.log(s); //throws error
 
+s[2] = 45;
+console.log(s); //[5, 6, 45]
+*/
+const s = [5, 7, 2];
+
+function editInPlace() {
+    s[0] = 2, s[1] = 5, s[2] = 7;
+    return s;
+}
+console.log(editInPlace());
 //ex.026
