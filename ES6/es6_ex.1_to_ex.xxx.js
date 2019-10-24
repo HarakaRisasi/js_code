9d3cfe22853bb58ab428b41270b212d8f0f8d16c
HarakaRisasi <harakarisasi@gmail.com>
2019-10-25 04:21:07 +1000

    ?getter_and_setter_are_basic_concept?

--- ES6/es6_ex.1_to_ex.xxx.js
+++ ES6/es6_ex.1_to_ex.xxx.js
@@ -755,5 +755,17 @@ const carrot = new Vegetable('carrot');
 console.log(carrot.name); //=> 'carrot'
 
 //ex.21
+/*
+Use getters and setters to Control Access to an Object
+
+You can obtain values from an object, and set a value of a property within an object.
+
+-Getter functions are meant to simply return (get) the value of an object's private variable to the user 
+without the user directly accessing the private variable.
+
+-Setter functions are meant to modify (set) the value of an object's private 
+variable based on the value passed into the setter function. 
+This change could involve calculations, or even overwriting the previous value completely.
+*/
 
 //ex.031
