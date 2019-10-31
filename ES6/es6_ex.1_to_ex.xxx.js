dbaa69611b89cd9824512f1696888e72e2d5f1c7
HarakaRisasi <harakarisasi@gmail.com>
2019-11-01 05:34:31 +1000

    >>_add.ex_030

--- ES6/es6_ex.1_to_ex.xxx.js
+++ ES6/es6_ex.1_to_ex.xxx.js
@@ -1018,7 +1018,7 @@ return x + y;
 -----------------------------------
 */
 export default function subtract(x, y) {
-  return x - y;
+    return x - y;
 }
 
 //ex.028
@@ -1038,7 +1038,7 @@ name here when importing a default.
 
 import subtract from "./math_functions.js";
 
-subtract(7,4);
+subtract(7, 4);
 
 //ex.029
 /*
@@ -1057,5 +1057,32 @@ const myPromise = new Promise((resolve, reject) => {
 const makeServerRequest = new Promise((resolve, reject) => {});
 
 //ex.030
+/*
+Complete a Promise with resolve and reject
+A promise has three states: pending, fulfilled, and rejected. 
+The promise you created in the last challenge is forever stuck in the pending 
+state because you did not add a way to complete the promise. 
+The resolve and reject parameters given to the promise argument are used to do this. 
+resolve is used when you want your promise to succeed, and reject is 
+used when you want it to fail.
+----------------------------------------------------
+const myPromise = new Promise((resolve, reject) => {
+  if(condition here) {
+    resolve("Promise was fulfilled");
+  } else {
+    reject("Promise was rejected");
+  }
+----------------------------------------------------
+*/
+const makeServerRequest = new Promise((resolve, reject) => {
+
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve('We got the data');
+    } else {
+        reject('Data not received');
+    }
+});
 
 //ex.031
