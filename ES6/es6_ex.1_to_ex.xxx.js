1b2b5b810bf6dc5c50e4535546e7402405ea4d1a
HarakaRisasi <harakarisasi@gmail.com>
2019-11-01 05:20:23 +1000

    >>_add.ex_028

--- ES6/es6_ex.1_to_ex.xxx.js
+++ ES6/es6_ex.1_to_ex.xxx.js
@@ -1022,5 +1022,24 @@ export default function subtract(x, y) {
 }
 
 //ex.028
+/*
+Import a Default Export
+In the last challenge, you learned about export default and its uses. 
+To import a default export, you need to use a different import syntax. 
+In the following example, add is the default export of 
+the math_functions.js file. 
+
+The syntax differs in one key place. 
+The imported value, add, is not surrounded by curly braces ({}). add here 
+is simply a variable name for whatever the default 
+export of the math_functions.js file is. You can use any 
+name here when importing a default.
+*/
+
+import subtract from "./math_functions.js";
+
+subtract(7,4);
+
+//ex.029
 
 //ex.031
