93346ba5313be1e432cb910b2e01b90276c1eb3b
HarakaRisasi <harakarisasi@gmail.com>
2019-11-04 06:30:05 +1000

    upgrade_code_add.ex_001_add_console.log()

--- regular_expression/ex.001_using_test_method.js
+++ regular_expression/ex.001_using_test_method.js
@@ -12,7 +12,8 @@ let testRegex = /Code/;
 testRegex.test(testStr); //=> true
 -----------------------------
 */
-
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); //=> true
+
+console.log(result);
