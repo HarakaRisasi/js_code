b799620d1bfa796a2cb7c2e3f3d833f2e8b399dc
HarakaRisasi <harakarisasi@gmail.com>
2019-11-04 16:04:36 +1000

    add.ex_005_extract_matches

--- regular_expression/ex.005_extract_matches.js
+++ regular_expression/ex.005_extract_matches.js
@@ -6,10 +6,12 @@ You can also extract the actual matches you found with the .match() method.
 let regEx = "Haraka Risasi!".match(/haraka/i);
 console.log(regEx); //=> ["Haraka"]
 ----------------------------------------------
+----------------------------------------------
 let string = "Regular Expressions";
 let regEx = /Expressions/i;
 
 console.log(string.match(regEx)); //=> ["Expressions"]
+----------------------------------------------
 */
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/i;
