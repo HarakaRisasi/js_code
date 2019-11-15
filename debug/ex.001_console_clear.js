d9cb9abfc4b2d22bd0e5c48d916809eb7a468bd8
HarakaRisasi <harakarisasi@gmail.com>
2019-11-15 10:29:57 +1000

    add.ex_001_console_clear

---
+++ debug/ex.001_console_clear.js
@@ -0,0 +1,9 @@
+// Open your browser console.
+let output = "Get this to log once in the browser console and twice in the freeCodeCamp console";
+// Use console.clear() on the next line to clear the browser console.
+console.clear()
+//=> Console was cleared
+
+// Use console.log() to print the output variable.
+console.log(output)
+//=> test.js:7 Get this to log once in the browser console and twice in the freeCodeCamp console
