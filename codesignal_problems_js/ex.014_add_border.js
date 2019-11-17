b1af85afbe0e691ae589aeabff155c970f4e3f3b
HarakaRisasi <harakarisasi@gmail.com>
2019-11-18 04:21:34 +1000

    add.ex_014_add_border_around_string_(not_my_answer)

---
+++ codesignal_problems_js/ex.014_add_border.js
@@ -0,0 +1,16 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+picture = ["abc",
+           "ded"]
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+*/
+addBorder = p => [L = p[0].replace(/./g, a = `*`), ...p, L].map(s => a + s + a);
+
+console.log(addBorder(["abc", "ded"])); //=> ["*****", "*abc*", "*ded*", "*****"]
+console.log(addBorder(["a"])); //=> ["***", "*a*", "***"]
+console.log(addBorder(["aa", "**", "zz"])); //=> ["****", "*aa*", "****", "*zz*", "****"]
+console.log(addBorder(["wzy**"])); //=> ["*******", "*wzy***", "*******"]
