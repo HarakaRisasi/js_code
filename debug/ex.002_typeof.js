2650213e73d2cd0523e31395c29c079fe384c8ed
HarakaRisasi <harakarisasi@gmail.com>
2019-11-19 23:36:29 +1000

    add.ex_002_check_type_of_variable

---
+++ debug/ex.002_typeof.js
@@ -0,0 +1,9 @@
+/*
+Check the Type of a Variable
+*/
+let seven = 7;
+let three = "3";
+
+console.log(seven + three) //=> 7'3'
+console.log(typeof seven); //=> "number"
+console.log(typeof three); //=> "string"
